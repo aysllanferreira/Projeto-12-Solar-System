9f05170a620eefc11b2712491189085d8a8c2818
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-21 15:02:20 -0300

    planetas na tela implementado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import SolarSystem from './components/SolarSystem ';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 export default class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map(({ name, image }) => (
+          <PlanetCard key={ name } planetName={ name } planetImage={ image } />
+        ))}
       </div>
     );
   }
