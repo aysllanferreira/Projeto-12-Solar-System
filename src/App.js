6bdc544be54adfead3a3263f0df610ceed1e1d3f
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-21 14:49:46 -0300

    title dentro de solar system

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem ';
-import Title from './components/Title';
 
 class App extends React.Component {
   render() {
@@ -10,7 +9,7 @@ class App extends React.Component {
         <p>Sistema Solar</p>
         <Header />
         <SolarSystem />
-        <Title headline="Teste" />
+
       </>
 
     );
--- src/components/SolarSystem .jsx
+++ src/components/SolarSystem .jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 export default class SolarSystem extends Component {
   render() {
     return (
-      <div data-testid="solar-system">SolarSystem </div>
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
     );
   }
 }
