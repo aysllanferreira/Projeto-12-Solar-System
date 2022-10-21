3b4dcbc2a4c697932840c0ce793837d4dfd02d75
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-21 14:52:59 -0300

    PlanetCard component implementado

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
