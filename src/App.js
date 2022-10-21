42da465979c3a1acdff408aef14ae4ae83ad04d3
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-21 14:47:52 -0300

    title implementado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem ';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
         <p>Sistema Solar</p>
         <Header />
         <SolarSystem />
+        <Title headline="Teste" />
       </>
 
     );
