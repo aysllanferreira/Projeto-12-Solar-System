eeff5101f03f7e79f55a50d09c3dc4f98709cc3a
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-21 15:04:32 -0300

    Tiitle chamado em Mission com a headline missoes

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 export default class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">Missions</div>
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
